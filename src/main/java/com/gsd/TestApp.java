7bd266ccfcf5f601767fc2ecb372433dac50f6ab
Tahaa Bijapuri <76931171+tbijapur@users.noreply.github.com>
2021-09-22 19:52:33 -0500

    Tahaa test

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -4,7 +4,7 @@ public class TestApp {
 
 
     public static void main(String[] args){
-        System.out.println("SE475/371 GSD Course \n");
+        System.out.println("Tahaa Bijapuri");
 
         //TODO Print your name like this "Name: Vahid Alizadeh"
 
