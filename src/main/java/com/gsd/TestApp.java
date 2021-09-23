6b8221a379b2d5ea366d13a6c638f25cb766bd9a
Tahaa Bijapuri <76931171+tbijapur@users.noreply.github.com>
2021-09-22 20:12:55 -0500

    Changed print statement

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -4,9 +4,10 @@ public class TestApp {
 
 
     public static void main(String[] args){
-        System.out.println("Tahaa Bijapuri");
+        System.out.println("SE475/371 GSD Course \n");
 
         //TODO Print your name like this "Name: Vahid Alizadeh"
+        System.out.println("Tahaa Bijapuri");
 
     }
 }
